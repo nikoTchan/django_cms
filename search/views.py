d6919c2b3a9634ff3eea1bf151e720aa2ca68849
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-03-19 02:10:17 +0100

    Improving the Search View: empty search - no results

--- search/views.py
+++ search/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render_to_response
 from django.contrib.flatpages.models import FlatPage 
 
 def search(request): 
-	query = request.GET['q']
-	results = FlatPage.objects.filter(content__icontains=query) 
-	return render_to_response('search/search.html',
+  query = request.GET.get('q', '')
+  results = []
+  if query:
+    results = FlatPage.objects.filter(content__icontains=query)
+  return render_to_response('search/search.html',
                             { 'query': query,
-                              'results': FlatPage.objects.filter(content__icontains=query) })
+                              'results': results })
