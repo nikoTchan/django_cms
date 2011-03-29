2b189333fb96b12dd4573d945de7cc321cd14166
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-03-30 01:04:44 +0200

    Prepopulated_fields: slug from title. Added Category Programming.

--- standalone_apps/coltrane/admin.py
+++ standalone_apps/coltrane/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from coltrane.models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
-  pass
+  prepopulated_fields = { 'slug': ['title'] }
 
 admin.site.register(Category, CategoryAdmin)
