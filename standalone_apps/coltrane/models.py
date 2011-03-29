e9519f03db6961f3f7aa136135756ce451e78878
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-03-30 01:19:43 +0200

    Added new category Learning. Added ordering and help_text in Category model.

--- standalone_apps/coltrane/models.py
+++ standalone_apps/coltrane/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-  title = models.CharField(max_length=250)
-  slug = models.SlugField(unique=True)
+  title = models.CharField(max_length=250, help_text='Maximum 250 characters.')
+  slug = models.SlugField(unique=True, help_text='Suggested value automatically generated from title. Must be unique.')
   description = models.TextField()
 
   class Meta:
+#    ordering = ['title']
     verbose_name_plural = "Categories"
 
   def __unicode__(self):
