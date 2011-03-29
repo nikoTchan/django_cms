a07f30dbe9c8063e2dbc84555ce464ab90119f18
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-03-30 01:13:21 +0200

    Configured fieldsets for Categories, first title and description, last more info slug.

--- standalone_apps/coltrane/admin.py
+++ standalone_apps/coltrane/admin.py
@@ -3,6 +3,10 @@ from coltrane.models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
   prepopulated_fields = { 'slug': ['title'] }
+  fieldsets = [
+      (None,               { 'fields': ['title', 'description'] }),
+      ('More information', { 'fields': ['slug'] }),
+  ]
   list_display = ('title', 'slug', 'description')
   search_fields = ['title']
 
