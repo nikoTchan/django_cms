e25b0b0e16c0b7d7d0aea32d51877110bf7a4b46
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-03-30 01:09:57 +0200

    list_display and search_fields for administrate Categories.

--- standalone_apps/coltrane/admin.py
+++ standalone_apps/coltrane/admin.py
@@ -3,5 +3,7 @@ from coltrane.models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
   prepopulated_fields = { 'slug': ['title'] }
+  list_display = ('title', 'slug', 'description')
+  search_fields = ['title']
 
 admin.site.register(Category, CategoryAdmin)
