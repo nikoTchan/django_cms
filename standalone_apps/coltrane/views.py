9427cd463491020c5e797b32b348b2895af0addf
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-04-25 01:26:03 +0200

    Add entry_detail.html, fix url for entry_detail, fix get_absolute_url in template entry_index.

--- standalone_apps/coltrane/views.py
+++ standalone_apps/coltrane/views.py
@@ -10,4 +10,7 @@ def entry_detail(request, year, month, day, slug):
   date_stamp = time.strptime(year+month+day, "%Y%b%d")
   pub_date = datetime.date(*date_stamp[:3])
   return render_to_response('coltrane/entry_detail.html',
-                            { 'entry': Entry.objects.get(pub_date__year=pub_date.year, pub_date__month=pub_date.month, pub_date__day=pub_date.day, slug=slug) })
+                            { 'entry': Entry.objects.get(pub_date__year=pub_date.year,
+                                                         pub_date__month=pub_date.month,
+                                                         pub_date__day=pub_date.day,
+                                                         slug=slug) })
