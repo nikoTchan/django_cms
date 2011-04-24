8f42463cb6cd5278c6aace594735f270db2e9042
Marc Nicolàs i Aragó <marc.niko@gmail.com>
2011-04-25 01:44:51 +0200

    entry_detail using django.views.generic.date_based with entry_info_dict defining queryset and date_field.

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls.defaults import *
 from cms import settings
+from coltrane.models import Entry
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 import os.path
+
+entry_info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'pub_date',
+}
 
 urlpatterns = patterns('',
     # Example:
@@ -20,6 +26,7 @@ urlpatterns = patterns('',
 		  {'document_root': os.path.abspath(os.path.join(settings.STATIC_DIR, 'js/tiny_mce')).replace('\\','/')}),
     (r'^search/$', 'cms.search.views.search'),
     (r'^weblog/$', 'coltrane.views.entries_index'),
-    (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'coltrane.views.entry_detail'),
+    (r'^weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+      'django.views.generic.date_based.object_detail', entry_info_dict),
     (r'', include('django.contrib.flatpages.urls')),
 )
